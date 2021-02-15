f51f82e28419f51c767b591a8b03eb4de8aa718f
Robin Singla <robin.singla@asahiindia.com>
2021-02-16 00:49:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashscreenanimation/splash.java
@@ -0,0 +1,44 @@
+package com.example.splashscreenanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+
+public class splash extends AppCompatActivity {
+
+    ImageView appname,splashimg;
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        appname = findViewById(R.id.app_name);
+        splashimg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        splashimg.animate().translationY(-2500).setDuration(1000).setStartDelay(5000);
+        appname.animate().translationY(2000).setDuration(1000).setStartDelay(5000);
+        lottieAnimationView.animate().translationY(1500).setDuration(1000).setStartDelay(5000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(splash.this,MainActivity.class));
+
+            }
+        },6000);
+
+    }
+}
